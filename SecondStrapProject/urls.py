90dede80db454279972f398c396b964941baf75b
Nithin <nithinraj07sachu@gmail.com>
2025-12-03 12:31:33 +0530

    fix : problem in the admin_category style issue is fixed

--- SecondStrapProject/urls.py
+++ SecondStrapProject/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import custom_inactive_account_view
 from accounts.views import google_callback_safe
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("defaultadmin/", admin.site.urls),
@@ -17,3 +19,7 @@ urlpatterns = [
     path("profile/", include("userFolder.userprofile.urls"), name="userprofile"),
     path("superuser/", include("Admin.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
