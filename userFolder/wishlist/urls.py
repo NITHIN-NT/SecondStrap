9c6f52523573dbd9b55fc1806a4be0c9f77859a4
Nithin <nithinraj07sachu@gmail.com>
2025-12-13 16:31:43 +0530

    feat : in wishlist move to cart and remove from wishlist

--- userFolder/wishlist/urls.py
+++ userFolder/wishlist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('',wishlistView,name='wishlist'),
-    path('add/',add_to_wishlist ,name='add_to_wishlist')
+    path('add/',add_to_wishlist ,name='add_to_wishlist'),
+    path('add/cart/',add_to_cart,name='add_to_cart'),
+    path('remove/',remove_from_wishlist,name='remove_from_wishlist')
 ]
--- userFolder/wishlist/views.py
+++ userFolder/wishlist/views.py
@@ -5,16 +5,22 @@ from django.http import HttpResponse,JsonResponse
 from django.views.decorators.http import require_POST
 from products.models import ProductVariant
 from .models import Wishlist,WishlistItem
+from userFolder.cart.models import Cart,CartItems
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 
 # Create your views here.
 def wishlistView(request):
-    wishlist = Wishlist.objects.get(user=request.user)
-    wishlist_items = WishlistItem.objects.filter(wishlist=wishlist).select_related('product','variant').order_by('-item_added')
+    wishlist_items = None
+    if request.user.is_authenticated:    
+        wishlist,_ = Wishlist.objects.get_or_create(user=request.user)
+        wishlist_items = WishlistItem.objects.filter(wishlist=wishlist).select_related('product','variant').order_by('-item_added')
     context = {
         'wishlist_items':wishlist_items
     }
     return render(request,'wishlist/wishlist.html',context)
 
+@login_required
 @require_POST
 def add_to_wishlist(request):
     try:
@@ -48,5 +54,61 @@ def add_to_wishlist(request):
     else:
         return JsonResponse({"status":"error","message":"Item already in the wishlist",},status=400)
     
-        
-        
+@login_required
+@require_POST    
+def add_to_cart(request):
+    try:
+        data = json.loads(request.body)
+        variant_id =data.get('variant_id')
+        size = data.get('size')
+    except json.JSONDecodeError:
+        return JsonResponse({"status":"error","message":"Data is not here"})
+    
+    if not variant_id:
+        return JsonResponse({"status":"error","message":"Variant Id is missing."})
+    
+    variant = get_object_or_404(ProductVariant,id=variant_id)
+    
+    if not variant.stock > 0:
+        return JsonResponse({"status":"error","message":"Sorry. Out of stock"})
+    
+    if not variant.product.is_active:
+        return JsonResponse({"status":"error","message":"Sorry. Not Available"})
+    
+    cart,_ = Cart.objects.get_or_create(user=request.user)
+    
+    productExists = CartItems.objects.filter(cart=cart,variant=variant).exists()
+    
+    if not productExists:
+        CartItems.objects.create(
+            cart=cart,
+            variant=variant,
+            size=size,
+            from_wishlist = True
+        )
+        wishlist_item = WishlistItem.objects.get(variant=variant)
+        wishlist_item.delete()
+        return JsonResponse({"status":"success","message":"Item moved to the cart"})
+    else:
+        return JsonResponse({"status":"error","message":"Item already in the cart"})
+    
+    
+@require_POST
+@login_required
+@transaction.atomic
+def remove_from_wishlist(request):
+    try:
+        data = json.loads(request.body)
+        item_id = data.get('item_id')
+    except json.JSONDecodeError:
+        return JsonResponse({"status":"error","message":"Data is not here "})
+    
+    try:
+        item = get_object_or_404(WishlistItem,id=item_id)
+        item.delete()
+        return JsonResponse({"status":"success","message":"Item removed from the wishlist !"})
+    except Exception as e:
+        print(str(e))
+        return JsonResponse({"status":"error","message":"Something went wrong"})
+
+    
