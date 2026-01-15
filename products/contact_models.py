9ea345c907fefe6fb9e36b5ebb956f1233b7726c
NITHIN-NT <nithinraj07sachu@gmail.com>
2026-01-15 18:50:16 +0530

    review: image file name issue fixed

--- products/contact_models.py
+++ products/contact_models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import os
+import uuid
 
 class ContactModel(models.Model):
     name = models.CharField(max_length=150, blank=True, null=True)
@@ -10,9 +12,13 @@ class ContactModel(models.Model):
     
     def __str__(self):
         return f'{self.name} messages : {self.message}'
-
+def get_file_path(instance,filename):
+    ext = filename.split('.')[-1]
+    
+    filename = f"{uuid.uuid4().hex[:10]}.{ext}"
+    return os.path.join('thumbanails/',filename)
 class Thumbanails(models.Model):
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to=get_file_path,blank=True, null=True)
     name = models.CharField(null=True,max_length=250)
     is_visible = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
