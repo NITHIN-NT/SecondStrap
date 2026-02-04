dc93a23b571f4f70ede4ce2de1a1d864ef6fa871
NITHIN-NT <nithinraj07sachu@gmail.com>
2026-02-04 09:38:35 +0530

    fix: corrected typo in verification_required import and updated decorator target

--- userFolder/checkout/views.py
+++ userFolder/checkout/views.py
@@ -10,7 +10,7 @@ from userFolder.order.models import OrderMain
 from coupon.models import Coupon
 from django.utils import timezone
 from userFolder.payment.utils import calculate_cart_totals, sync_draft_order
-from userFolder.cart.utils import verification_requried
+from userFolder.cart.utils import verification_required
 from django.utils.decorators import method_decorator
 
 @method_decorator(verification_required, name='dispatch')
